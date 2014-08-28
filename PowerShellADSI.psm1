a68ebe5ab89ba99fb03b12f316706584e7aef612
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2014-08-28 00:20:37 -0400

    Update Get-ADSIDomainUser

--- PowerShellADSI.psm1
+++ PowerShellADSI.psm1
@@ -9,39 +9,80 @@ function Get-ADSIDomainUser
 	Specify the DisplayName of the user
 .PARAMETER DistinguishedName
 	Specify the DistinguishedName path of the user
+.PARAMETER Credential
+    Specify the Credential to use for the query
+.PARAMETER SizeLimit
+    Specify the number of item maximum to retrieve
+.PARAMETER DomainDistinguishedName
+    Specify the Domain or Domain DN path to use
 .EXAMPLE
 	Get-ADSIDomainUser -SamAccountName fxcat
 .EXAMPLE
 	Get-ADSIDomainUser -DisplayName "Cat, Francois-Xavier"
 .EXAMPLE
 	Get-ADSIDomainUser -DistinguishedName "CN=Cat\, Francois-Xavier,OU=Admins,DC=FX,DC=local"
+.EXAMPLE
+    Get-ADSIDomainUser -SamAccountName testuser -Credential (Get-Credential -Credential Msmith)
 #>
 	[CmdletBinding()]
 	PARAM (
 		[Parameter(ParameterSetName = "DisplayName")]
 		[String]$DisplayName,
+
 		[Parameter(ParameterSetName = "SamAccountName")]
 		[String]$SamAccountName,
+
 		[Parameter(ParameterSetName = "DistinguishedName")]
-		[String]$DistinguishedName
+		[String]$DistinguishedName,
+
+        [Alias("RunAs")]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty,
+
+		[Parameter()]
+		[Alias("DomainDN","Domain")]
+        [String]$DomainDistinguishedName=$(([adsisearcher]"").Searchroot.path),
+
+        [Alias("ResultLimit","Limit")]
+		[int]$SizeLimit='100'
+        
 	)
 	BEGIN { }
 	PROCESS
 	{
 		TRY
 		{
+            		# Building the basic search object with some parameters
+			$Search = New-Object -TypeName System.DirectoryServices.DirectorySearcher -ErrorAction 'Stop'
+            		$Search.SizeLimit = $SizeLimit
+			$Search.SearchRoot = $DomainDN
+
 			If ($DisplayName)
 			{
-				$Search = [adsisearcher]"(&(objectCategory=person)(objectClass=User)(displayname=$DisplayName))"
+				$Search.filter = "(&(objectCategory=person)(objectClass=User)(displayname=$DisplayName))"
 			}
 			IF ($SamAccountName)
 			{
-				$Search = [adsisearcher]"(&(objectCategory=person)(objectClass=User)(samaccountname=$SamAccountName))"
+				$Search.filter = "(&(objectCategory=person)(objectClass=User)(samaccountname=$SamAccountName))"
 			}
 			IF ($DistinguishedName)
 			{
-				$Search = [adsisearcher]"(&(objectCategory=person)(objectClass=User)(distinguishedname=$distinguishedname))"
+				$Search.filter = "(&(objectCategory=person)(objectClass=User)(distinguishedname=$distinguishedname))"
 			}
+            
+			IF ($DomainDistinguishedName)
+			{
+			IF ($DomainDistinguishedName -notlike "LDAP://*") {$DomainDistinguishedName = "LDAP://$DomainDistinguishedName"}#IF
+			Write-Verbose -Message "Different Domain specified: $DomainDistinguishedName"
+					$Search.SearchRoot = $DomainDistinguishedName
+			}
+			
+			IF ($PSBoundParameters['Credential'])
+			{
+			$Cred = New-Object -TypeName System.DirectoryServices.DirectoryEntry -ArgumentList $DomainDistinguishedName,$($Credential.UserName),$($Credential.GetNetworkCredential().password)
+			$Search.SearchRoot = $Cred
+			}
+
 			foreach ($user in $($Search.FindAll()))
 			{
 				
@@ -53,13 +94,93 @@ function Get-ADSIDomainUser
 					"Description" = $user.properties.description -as [string]
 					"DistinguishedName" = $user.properties.distinguishedname -as [string]
 					"ADsPath" = $user.properties.adspath -as [string]
-                    			"MemberOf" = $user.properties.memberof
-				}
+					"MemberOf" = $user.properties.memberof
+					"Location" = $user.properties.l -as [string]
+					"Country" = $user.properties.co -as [string]
+					"PostalCode" = $user.Properties.postalcode -as [string]
+					"Mail" = $user.properties.mail -as [string]
+					"TelephoneNumber" = $user.properties.telephonenumber -as [string]
+					"LastLogonTimeStamp" = $user.properties.lastlogontimestamp  -as [string]
+					"ObjectCategory" = $user.properties.objectcategory -as [string]
+					"Manager" = $user.properties.manager -as [string]
+					"HomeDrive" = $user.properties.homedrive -as [string]
+					"LogonCount" = $user.properties.logoncount -as [string]
+					"DirectReport" = $user.properties.l -as [string]
+					"useraccountcontrol" = $user.properties.useraccountcontrol -as [string]
+					
+					<#
+					lastlogoff
+					codepage
+					c
+					department
+					msexchrecipienttypedetails
+					primarygroupid
+					objectguid
+					title
+					msexchumdtmfmap
+					whenchanged
+					distinguishedname
+					targetaddress
+					homedirectory
+					dscorepropagationdata
+					msexchremoterecipienttype
+					instancetype
+					memberof
+					usnchanged
+					usncreated
+					description
+					streetaddress
+					directreports
+					samaccountname
+					logoncount
+					co
+					msexchpoliciesexcluded
+					userprincipalname
+					countrycode
+					homedrive
+					manager
+					samaccounttype
+					showinaddressbook
+					badpasswordtime
+					msexchversion
+					objectsid
+					objectclass
+					proxyaddresses
+					objectcategory
+					extensionattribute3
+					givenname
+					pwdlastset
+					managedobjects
+					st
+					whencreated
+					physicaldeliveryofficename
+					legacyexchangedn
+					lastlogon
+					accountexpires
+					useraccountcontrol
+					badpwdcount
+					postalcode
+					displayname
+					msexchrecipientdisplaytype
+					sn
+					mail
+					lastlogontimestamp
+					company
+					adspath
+					name
+					telephonenumber
+					cn
+					msexchsafesendershash
+					mailnickname
+					l
+					
+					#>
+				}#Properties
 				
 				# Output the info
 				New-Object -TypeName PSObject -Property $Properties
-			}
-		}
+			}#FOREACH
+		}#TRY
 		CATCH
 		{
 			Write-Warning -Message "[PROCESS] Something wrong happened!"
