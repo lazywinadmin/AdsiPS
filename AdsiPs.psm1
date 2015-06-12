704bded3345fa052b35d58d2e37963acb5aa6a5c
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-06-12 13:38:38 -0400

    Add DirectoryContext functions
    -New-ADSIDirectoryContextDomain
    -New-ADSIDirectoryContextForest

--- AdsiPs.psm1
+++ AdsiPs.psm1
@@ -2337,6 +2337,141 @@ function Get-ADSIUser
 	END { Write-Verbose -Message "[END] Function Get-ADSIUser End." }
 }
 
+Function New-ADSIDirectoryContextDomain
+{
+<#
+    .DESCRIPTION
+		Function to create an Active Directory Domain DirectoryContext object
+
+	.SYNOPSIS
+        Function to create an Active Directory Domain DirectoryContext object
+
+	.PARAMETER DomainName
+		Specifies the domain to query.
+		Default is the current domain.
+
+	.PARAMETER Credential
+		Specifies the alternative credentials to use.
+		It will use the current credential if not specified.
+
+	.EXAMPLE
+        New-ADSIDirectoryContextDomain
+	
+    .EXAMPLE
+        New-ADSIDirectoryContextDomain -DomainName "Contoso.com" -Cred (Get-Credential)
+        
+    .EXAMPLE
+        $Domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($(New-ADSIDirectoryContextDomain -Credential LazyWinAdmin\francois-xavier.cat))
+        $Domain.DomainControllers
+        $Domain.InfrastructureRoleOwner
+
+	.NOTES
+        Francois-Xavier.Cat
+        LazyWinAdmin.com
+        @lazywinadm
+#>
+	
+	[CmdletBinding()]
+	PARAM (
+		[Alias("RunAs")]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty,
+		
+		$DomainName = [System.DirectoryServices.ActiveDirectory.Domain]::Getcurrentdomain()
+		
+	)
+	PROCESS
+	{
+		# ContextType = Domain
+		$ContextType = [System.DirectoryServices.ActiveDirectory.DirectoryContextType]::Domain
+		
+		TRY
+		{
+			IF ($PSBoundParameters['Credential'])
+			{
+				# Query the specified domain or current if not entered, with the specified credentials
+				New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ContextType, $DomainName, $($Credential.UserName), $($Credential.GetNetworkCredential().password)
+			}
+			ELSE
+			{
+				# Query the specified domain or current if not entered, with the current credentials
+				New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ContextType, $DomainName
+			}
+		}#TRY
+		CATCH
+		{
+			
+		}
+	}#PROCESS
+}
+
+Function New-ADSIDirectoryContextForest
+{
+<#
+    .DESCRIPTION
+		Function to create an Active Directory Forest DirectoryContext object
+
+	.SYNOPSIS
+        Function to create an Active Directory Forest DirectoryContext object
+
+	.PARAMETER ForestName
+		Specifies the forest to query.
+		Default is the current forest.
+
+	.PARAMETER Credential
+		Specifies the alternative credentials to use.
+		It will use the current credential if not specified.
+
+	.EXAMPLE
+        New-ADSIDirectoryContextForest
+	
+    .EXAMPLE
+        New-ADSIDirectoryContextForest -ForestName "Contoso.com" -Cred (Get-Credential)
+        
+    .EXAMPLE
+        $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetForest($(New-ADSIDirectoryContextForest -Credential LazyWinAdmin\francois-xavier.cat)))
+        $Forest.FindGlobalCatalog()
+
+	.NOTES
+        Francois-Xavier.Cat
+        LazyWinAdmin.com
+        @lazywinadm
+#>
+	
+	[CmdletBinding()]
+	PARAM (
+		[Alias("RunAs")]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty,
+		
+		$ForestName = [System.DirectoryServices.ActiveDirectory.Forest]::Getcurrentforest()
+		
+	)
+	PROCESS
+	{
+		# ContextType = Domain
+		$ContextType = [System.DirectoryServices.ActiveDirectory.DirectoryContextType]::Forest
+		
+		TRY
+		{
+			IF ($PSBoundParameters['Credential'])
+			{
+				# Query the specified domain or current if not entered, with the specified credentials
+				New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ContextType, $ForestName, $($Credential.UserName), $($Credential.GetNetworkCredential().password)
+			}
+			ELSE
+			{
+				# Query the specified domain or current if not entered, with the current credentials
+				New-Object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ContextType, $ForestName
+			}
+		}#TRY
+		CATCH
+		{
+			
+		}
+	}#PROCESS
+}
+
 function Remove-ADSIGroupMember
 {
 <#
