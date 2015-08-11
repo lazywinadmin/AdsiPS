1a20224c2030ecd91a70fb8227685866ea0214a4
Micky Balladelli <micky@balladelli.com>
2015-08-11 17:56:31 +0200

    New AD replication cmdlets

---
+++ Get-ADSISitesInfo.ps1
@@ -0,0 +1,78 @@
+﻿<#  
+.SYNOPSIS  
+    Get-ADSISitesInfo returns information about the connected DC's Sites.
+
+.DESCRIPTION  
+
+      Get-ADSISitesInfo returns information about the connected DC's Sites.
+
+      
+.PARAMETER ComputerName
+
+    Defines the remote computer to connect to.
+
+.PARAMETER Credential
+
+    Defines alternate credentials to use. Use Get-Credential to create proper credentials.
+
+
+.EXAMPLE
+
+      Get-ADSISitesInfo -ComputerName dc1.ad.local
+
+        Name                           : Default-First-Site-Name
+        Domains                        : {ad.local}
+        Subnets                        : {}
+        Servers                        : {DC1.ad.local, DC2.ad.local}
+        AdjacentSites                  : {}
+        SiteLinks                      : {DEFAULTIPSITELINK}
+        InterSiteTopologyGenerator     : DC1.ad.local
+        Options                        : None
+        Location                       : 
+        BridgeheadServers              : {}
+        PreferredSmtpBridgeheadServers : {}
+        PreferredRpcBridgeheadServers  : {}
+        IntraSiteReplicationSchedule   : System.DirectoryServices.ActiveDirectory.ActiveDirectorySchedule
+
+      Connects to remote domain controller dc1.ad.local using current credentials retrieves site information.
+
+
+.NOTES  
+    Filename    : Get-ADSISitesInfo.ps1
+    Author      : Micky Balladelli micky@balladelli.com  
+
+.LINK  
+    https://github.com/lazywinadmin/AdsiPS
+#> 
+function Get-ADSISitesInfo
+{
+      [CmdletBinding()] 
+      param ( [Parameter(Mandatory=$true)][string] $ComputerName = $null,
+              [Management.Automation.PSCredential] $Credential = $null
+              )
+                  
+    if ($ComputerName)
+    {
+        if ($Credential)
+        {
+            $context = new-object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList "DirectoryServer",$ComputerName,$Credential.UserName,$Credential.GetNetworkCredential().Password
+        }    
+        else
+        {
+            $context = new-object -TypeName System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList "DirectoryServer",$ComputerName
+        }
+    }
+
+    if ($context)
+    {
+        Write-Verbose -Message "Connecting to $ComputerName"
+        $dc = [System.DirectoryServices.ActiveDirectory.DomainController]::GetDomainController($context) 
+    }
+
+    if ($dc)
+    {
+        Write-Verbose -Message "Information about forest $($dc.forest.name)"
+        $dc.forest.sites | ForEach-Object {$_}
+    }
+}
+
