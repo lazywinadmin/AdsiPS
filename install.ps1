69b5fa42757e4e0c0012d5152cc6d35ad3031be9
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-02-25 12:19:36 -0500

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,33 @@
+param([string]$InstallDirectory)
+
+$ModuleName = "AdsiPS"
+
+$fileList = echo `
+    "$($ModuleName).psd1" `
+    "$($ModuleName).psm1"
+    
+
+if ('' -eq $InstallDirectory)
+{
+    $personalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+
+    if (($env:PSModulePath -split ';') -notcontains $personalModules) {
+        Write-Warning "$personalModules is not in `$env:PSModulePath"
+    }
+
+    if (-not(Test-Path $personalModules)) {
+        Write-Error "$personalModules does not exist"
+    }
+
+    $InstallDirectory = Join-Path -Path $personalModules -ChildPath $ModuleName
+}
+
+if (-not(Test-Path $InstallDirectory)) {
+    $null = mkdir $InstallDirectory
+}
+
+$WebClient = New-Object -typename System.Net.WebClient
+$fileList |
+    ForEach-Object {
+        $WebClient.DownloadFile("https://raw.github.com/LazyWinAdmin/$ModuleName/master/$_","$installDirectory\$_")
+    }
