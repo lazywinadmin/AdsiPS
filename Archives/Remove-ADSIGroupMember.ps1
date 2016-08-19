ed010cfc30b0423d2c82556c8b3d104878023062
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2016-08-19 00:07:47 -0400

    Archive Remove-ADSIGroupMember

---
+++ Archives/Remove-ADSIGroupMember.ps1
@@ -0,0 +1,33 @@
+﻿function Remove-ADSIGroupMember
+{
+<#
+.SYNOPSIS
+    This function will remove Domain user from a Domain Group
+.EXAMPLE
+    Remove-ADSIGroupMember -GroupSamAccountName TestGroup -UserSamAccountName Fxcat
+
+    This will remove the domain user fxcat from the group TestGroup
+	
+.NOTES
+	Francois-Xavier Cat
+	LazyWinAdmin.com
+	@lazywinadm
+#>
+	[CmdletBinding()]
+	PARAM ($GroupSamAccountName,
+		
+		$UserSamAccountName)
+	$UserInfo = [ADSI]"$((Get-ADSIUser -SamAccountName $UserSamAccountName).AdsPath)"
+	$GroupInfo = [ADSI]"$((Get-ADSIGroup -SamAccountName $GroupSamAccountName).AdsPath)"
+	
+	IF (Test-ADSIUserIsGroupMember -GroupSamAccountName $GroupSamAccountName -UserSamAccountName $UserSamAccountName)
+	{
+		Write-Verbose "Removing $UserSamAccountName from $GroupSamAccountName"
+		$GroupInfo.Remove($UserInfo.ADsPath)
+	}
+	ELSE
+	{
+		
+		Write-Verbose "$UserSamAccountName is not member of $GroupSamAccountName"
+	}
+}
