b73adc7eee32fb0483bd514b6cf062fa44ca548d
Christophe Kumor <christophekumor@users.noreply.github.com>
2017-04-24 15:29:32 +0200

    Get-ADSIPrintQueue

---
+++ AdsiPS/Public/Get-ADSIPrintQueue.ps1
@@ -0,0 +1,109 @@
+﻿function Get-ADSIPrintQueue
+{
+<#
+.SYNOPSIS
+	Function to retrieve a PrintQueue in Active Directory
+
+.DESCRIPTION
+	Function to retrieve a PrintQueue in Active Directory
+
+.PARAMETER  printerName
+	Specify the printerName of PrintQueue.
+	
+.PARAMETER Credential
+    Specify the Credential to use
+
+.PARAMETER DomainDistinguishedName
+    Specify the DistinguishedName of the Domain to query
+	
+.PARAMETER SizeLimit
+    Specify the number of item(s) to output
+	
+.EXAMPLE
+	Get-ADSIPrintQueue -printerName MyPrinter
+
+.NOTES
+	Christophe Kumor
+
+	github.com/lazywinadmin/AdsiPS
+#>
+	
+	[CmdletBinding()]
+	PARAM (
+		[Parameter(ParameterSetName = "printerName")]
+		[String]$printerName,
+		
+		[Parameter(ValueFromPipelineByPropertyName = $true)]
+		[Alias("Domain", "DomainDN", "SearchRoot", "SearchBase")]
+		[String]$DomainDistinguishedName = $(([adsisearcher]"").Searchroot.path),
+		
+		[Alias("RunAs")]
+		[System.Management.Automation.PSCredential]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty,
+		
+		[Alias("ResultLimit", "Limit")]
+		[int]$SizeLimit = '100'
+	)
+	BEGIN { }
+	PROCESS
+	{
+		TRY
+		{
+			# Building the basic search object with some parameters
+			$Search = New-Object -TypeName System.DirectoryServices.DirectorySearcher -ErrorAction 'Stop'
+			$Search.SizeLimit = $SizeLimit
+			$Search.SearchRoot = $DomainDistinguishedName
+			
+			$Search.filter = "(&(objectClass=printQueue)(printerName=$printerName))"
+			
+
+			IF ($PSBoundParameters['DomainDistinguishedName'])
+			{
+				IF ($DomainDistinguishedName -notlike "LDAP://*") { $DomainDistinguishedName = "LDAP://$DomainDistinguishedName" }#IF
+				Write-Verbose -Message "Different Domain specified: $DomainDistinguishedName"
+				$Search.SearchRoot = $DomainDistinguishedName
+			}
+			IF ($PSBoundParameters['Credential'])
+			{
+				$Cred = New-Object -TypeName System.DirectoryServices.DirectoryEntry -ArgumentList $DomainDistinguishedName, $($Credential.UserName), $($Credential.GetNetworkCredential().password)
+				$Search.SearchRoot = $Cred
+			}
+			
+			foreach ($Object in $($Search.FindAll()))
+			{
+				# Define the properties
+				#  The properties need to be lowercase!!!!!!!!
+				$Properties = @{
+					"DisplayName" = $Object.properties.displayname -as [string]
+					"Name" = $Object.properties.name -as [string]
+					"printerName" = $Object.properties.printername -as [string]
+					"location" = $Object.properties.location -as [string]
+					"Description" = $Object.properties.description -as [string]
+					"portName" = $Object.properties.portname -as [string] 
+					"driverName"  = $Object.properties.drivername -as [string] 
+					"ObjectCategory" = $Object.properties.objectcategory -as [string]
+					"ObjectClass" = $Object.properties.objectclass -as [string]
+					"DistinguishedName" = $Object.properties.distinguishedname -as [string]
+					"WhenCreated" = $Object.properties.whencreated -as [string]
+					"WhenChanged" = $Object.properties.whenchanged -as [string]
+					"serverName" = $Object.properties.servername -as [string]
+					"uNCName" = $Object.properties.uncname -as [string]
+					"printShareName" = $Object.properties.printsharename -as [string]
+				}
+				
+				# Output the info
+				New-Object -TypeName PSObject -Property $Properties
+			}
+		}
+		CATCH
+		{
+			Write-Warning -Message "[PROCESS] Something wrong happened!"
+			Write-Warning -Message $error[0].Exception.Message
+		}
+	}
+	END
+	{
+		Write-Verbose -Message "[END] Function Get-ADSIPrintQueue End."
+	}
+}
