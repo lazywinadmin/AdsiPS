b2c001c49812132f5d4f3e53156f502bf8975f33
François-Xavier Cat <fxcat@lazywinadmin.com>
2017-05-07 00:50:07 -0400

    Update verbose messages and output object

--- AdsiPS/Public/Get-ADSIGroupPolicyObject.ps1
+++ AdsiPS/Public/Get-ADSIGroupPolicyObject.ps1
@@ -1,4 +1,4 @@
-﻿function Get-ADSIGroupPolicyObject
+function Get-ADSIGroupPolicyObject
 {
 <#
 .SYNOPSIS
@@ -8,10 +8,10 @@
 	This function will query Active Directory Group Policy Objects
 	
 .PARAMETER Credential
-    Specify the Credential to use
+    Specifies alternative Credential to use
 	
 .PARAMETER DomainDistinguishedName
-    Specify the DistinguishedName of the Domain to query
+    Specify the DistinguishedName of the Domain to query.
 	
 .PARAMETER SizeLimit
     Specify the number of item(s) to output.
@@ -23,10 +23,9 @@
 	Retrieve all the group policy in the current domain	
 
 .NOTES
-	Francois-Xavier Cat
-	LazyWinAdmin.com
-	@lazywinadm
 	github.com/lazywinadmin/AdsiPS
+
+	https://msdn.microsoft.com/en-us/library/cc232507.aspx
 #>
 	[CmdletBinding()]
 	PARAM (
@@ -47,46 +46,79 @@
 	{
 		TRY
 		{
+			$ScriptName = (Get-Variable -name MyInvocation -Scope 0 -ValueOnly).MyCommand
+
 			# Building the basic search object with some parameters
+			Write-Verbose -message "[$ScriptName] Create DirectorySearcher"
 			$Search = New-Object -TypeName System.DirectoryServices.DirectorySearcher -ErrorAction 'Stop'
+			Write-Verbose -message "[$ScriptName] Set SizeLimit of '$SizeLimit'"
 			$Search.SizeLimit = $SizeLimit
+			Write-Verbose -message "[$ScriptName] Set Filter '(objectCategory=groupPolicyContainer)'"
 			$Search.Filter = "(objectCategory=groupPolicyContainer)"
 			
 			IF ($PSBoundParameters['DomainDistinguishedName'])
 			{
-				IF ($DomainDistinguishedName -notlike "LDAP://*") { $DomainDistinguishedName = "LDAP://$DomainDistinguishedName" }#IF
-				Write-Verbose -Message "[PROCESS] Different Domain specified: $DomainDistinguishedName"
+				Write-Verbose -message "[$ScriptName] DomainDistinguishedName specified = '$DomainDistinguishedName'"
+				IF ($DomainDistinguishedName -notlike "LDAP://*") {
+					Write-Verbose -message "[$ScriptName] DomainDistinguishedName notlike 'LDAP://*', prepending LDAP://"
+					$DomainDistinguishedName = "LDAP://$DomainDistinguishedName"
+				}#IF
 				$Search.SearchRoot = $DomainDistinguishedName
 			}
 			IF ($PSBoundParameters['Credential'])
 			{
-				Write-Verbose -Message "[PROCESS] Different Credential specified: $($credential.username)"
+				Write-Verbose -message "[$ScriptName] Different Credential specified: '$($credential.username)'"
 				$Cred = New-Object -TypeName System.DirectoryServices.DirectoryEntry -ArgumentList $DomainDistinguishedName, $($Credential.UserName), $($Credential.GetNetworkCredential().password)
 				$Search.SearchRoot = $Cred
 			}
 			If (-not $PSBoundParameters["SizeLimit"])
 			{
-				Write-Warning -Message "Default SizeLimit: 100 Results"
+				Write-Warning -Message "[$ScriptName] Default SizeLimit: 100 Results"
 			}
-			
+
+			Write-Verbose -message "[$ScriptName] Looking for objects..."
 			foreach ($GPO in $($Search.FindAll()))
 			{
 				# Define the properties
 				#  The properties need to be lowercase!!!!!!!!
-				$GPO.properties
-				
+				$Properties = @{
+					adspath                 = $gpo.properties.adspath                 -as [system.string]
+					gpcfunctionalityversion = $gpo.properties.gpcfunctionalityversion -as [system.int32]
+					usnchanged              = $gpo.properties.usnchanged              -as [system.string]
+					showinadvancedviewonly  = $gpo.properties.showinadvancedviewonly  -as [system.string]
+					displayname             = $gpo.properties.displayname             -as [system.string]
+					whencreated             = $gpo.properties.whencreated             -as [System.DateTime]
+					gpcmachineextensionnames= $gpo.properties.gpcmachineextensionnames-as [system.string]
+					instancetype            = $gpo.properties.instancetype            -as [system.string]
+					versionnumber           = $gpo.properties.versionnumber           -as [system.int32]
+					gpcfilesyspath          = $gpo.properties.gpcfilesyspath          -as [system.string]
+					usncreated              = $gpo.properties.usncreated              -as [system.string]
+					flags                   = $gpo.properties.flags                   -as [system.int32]
+					whenchanged             = $gpo.properties.whenchanged             -as [System.DateTime]
+					cn                      = $gpo.properties.cn                      -as [system.string]
+					objectguid              = $gpo.properties.objectguid
+					distinguishedname       = $gpo.properties.distinguishedname       -as [system.string]
+					objectcategory          = $gpo.properties.objectcategory          -as [system.string]
+					iscriticalsystemobject  = $gpo.properties.iscriticalsystemobject  -as [system.string]
+					objectclass             = $gpo.properties.objectclass
+					systemflags             = $gpo.properties.systemflags             -as [system.string]
+					dscorepropagationdata   = $gpo.properties.dscorepropagationdata   -as [system.string]
+					name                    = $gpo.properties.name                    -as [system.string]
+				}
+
 				# Output the info
-				#New-Object -TypeName PSObject -Property $Properties
+				New-Object -TypeName PSObject -Property $Properties
+				
 			}
 		}#TRY
 		CATCH
 		{
-			Write-Warning -Message "[PROCESS] Something wrong happened!"
-			Write-Warning -Message $error[0].Exception.Message
+			Write-Warning -Message "[$ScriptName] Something wrong happened!"
+			$PSCmdlet.ThrowTerminatingError($_)
 		}
 	}#PROCESS
 	END
 	{
-		Write-Verbose -Message "[END] Function Get-ADSISite End."
+		Write-Verbose -message "[$ScriptName] End."
 	}
 }
