e04b90e4ca21fd412ac831a0a2a78b2a622472f1
olivier.miossec <olivier.miossec@metsys.fr>
2019-07-08 15:47:21 +0200

    Add Get-ADSIDomainBackup.ps1 to retreive Backup Information from ADSI

---
+++ src/public/Get-ADSIDomainBackup.ps1
@@ -0,0 +1,99 @@
+function Get-ADSIDomainBackup {
+<#
+.SYNOPSIS
+    Function to retrieve last backup for each partitions of a given domain
+
+.DESCRIPTION
+    Function to retrieve last backup for each partitions of a given domain.
+    It look at the dsaSignature for each naming context.
+    dsaSignature attribute change after a succesfull backup
+
+.PARAMETER Credential
+    Specifies alternative credential to use
+
+.PARAMETER DomainName
+    Specifies the DomainName to query
+
+.EXAMPLE
+    get-ADSIDomainBackup
+
+    Retrieve Backup information for the current domain
+
+.EXAMPLE
+    get-ADSIDomainBackup -DomainName mytest.local
+
+    Retrieve Backup information for the domain mytest.local
+
+.EXAMPLE
+    get-ADSIDomainBackup -Credential (Get-Credential superAdmin) -Verbose
+
+    Retrieve Backup information for the current domain with the specified credential.
+
+.EXAMPLE
+    get-ADSIDomainBackup -DomainName mytest.local -Credential (Get-Credential superAdmin) -Verbose
+
+    Retrieve Backup information for the domain mytest.local with the specified credential.
+
+.NOTES
+    https://github.com/lazywinadmin/ADSIPS
+
+.OUTPUTS
+    'Ps'
+
+
+#>
+    [cmdletbinding()]
+    [OutputType('System.DirectoryServices.ActiveDirectory.Domain')]
+    param (
+        [Alias("RunAs")]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        $DomainName = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().name
+    )
+
+
+
+    process {
+        try {
+
+            $ContextObjectType = [System.DirectoryServices.ActiveDirectory.DirectoryContextType]::Domain
+
+            if ($PSBoundParameters['Credential']){
+                $ContextObject = new-object System.DirectoryServices.ActiveDirectory.DirectoryContext($ContextObjectType, $DomainName, $Credential.UserName, $Credential.GetNetworkCredential().password)
+            } else {
+
+            }
+
+
+
+            $DomainControlerObject = [System.DirectoryServices.ActiveDirectory.DomainController]::findOne($ContextObject)
+
+            $ActiveDirectoryPartitions = $DomainControlerObject.Partitions
+
+            $ArrayListBackupInfo = [System.Collections.ArrayList]::new()
+
+            foreach ($partition in $ActiveDirectoryPartitions) {
+
+                $ActiveDirectoryPartitionMetaData = $domainController.GetReplicationMetadata($partition)
+                $DsaSignatureAtribute = $ActiveDirectoryPartitionMetaData.item("dsaSignature")
+                $LastBackupDate = $DsaSignatureAtribute.LastOriginatingChangeTime.DateTime
+
+                $PartitionBackupInfo =   [pscustomobject]@{
+                    PartitionName=$partition
+                    BackupDateTime=$LastBackupDate
+                }
+
+                [void]$ArrayListBackupInfo.Add($PartitionBackupInfo)
+            }
+
+            return $ArrayListBackupInfo
+
+        }
+        catch {
+            $pscmdlet.ThrowTerminatingError($_)
+        }
+    }
+
+}
