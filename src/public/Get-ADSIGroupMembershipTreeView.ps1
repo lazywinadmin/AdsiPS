f869789fd646fc4a4072328c5804164bc6975964
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-15 17:25:22 -0800

    Replace tabs per spaces

--- src/public/Get-ADSIGroupMembershipTreeView.ps1
+++ src/public/Get-ADSIGroupMembershipTreeView.ps1
@@ -1,107 +1,107 @@
 function Get-ADSIGroupMembershipTreeView{
 <#
 .SYNOPSIS
-	Function to get all Group Memberships of a User or Computer including all Groups inherited by another group in a Tree View in Active Directory
+    Function to get all Group Memberships of a User or Computer including all Groups inherited by another group in a Tree View in Active Directory
 .DESCRIPTION
-	Function to get all Group Memberships of a User or Computer including all Groups inherited by another group in a Tree View in Active Directory
+    Function to get all Group Memberships of a User or Computer including all Groups inherited by another group in a Tree View in Active Directory
 .PARAMETER Identity
-	Specifies the Identity of the Source User or Computer
-	You can provide one of the following properties
-	DistinguishedName
-	Guid
-	Name
-	SamAccountName
-	Sid
-	UserPrincipalName
-	Those properties come from the following enumeration:
-	System.DirectoryServices.AccountManagement.IdentityType
+    Specifies the Identity of the Source User or Computer
+    You can provide one of the following properties
+    DistinguishedName
+    Guid
+    Name
+    SamAccountName
+    Sid
+    UserPrincipalName
+    Those properties come from the following enumeration:
+    System.DirectoryServices.AccountManagement.IdentityType
 .PARAMETER DomainName
-	Specifies the Domain Name where the function should look
+    Specifies the Domain Name where the function should look
 .PARAMETER Credential
-	Specifies the alternative credential to use.
-	By default it will use the current user windows credentials.
+    Specifies the alternative credential to use.
+    By default it will use the current user windows credentials.
 .EXAMPLE
     Get-ADSIGroupMembershipTreeView -Identity user1
 .EXAMPLE
-	Get-ADSIGroupMembershipTreeView -Identity computer1
+    Get-ADSIGroupMembershipTreeView -Identity computer1
 .NOTES
-	https://github.com/lazywinadmin/ADSIPS
+    https://github.com/lazywinadmin/ADSIPS
 .LINK
-	https://msdn.microsoft.com/en-us/library/System.DirectoryServices.AccountManagement.UserPrincipal(v=vs.110).aspx
+    https://msdn.microsoft.com/en-us/library/System.DirectoryServices.AccountManagement.UserPrincipal(v=vs.110).aspx
 #>
-	[CmdletBinding(SupportsShouldProcess = $true)]
-	param(
-	    [Parameter(Mandatory = $true,
-	        Position = 0,
-	        ParameterSetName = "Identity")]
-	    [System.string]$Identity,
-	
-	    [Alias("RunAs")]
-	    [System.Management.Automation.PSCredential]
-	    [System.Management.Automation.Credential()]
-	    $Credential = [System.Management.Automation.PSCredential]::Empty,
-	
-	    [System.String]$DomainName
-	)
-	
-	begin{
-	    $FunctionName = (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly).Mycommand
-	
-	    # Create Context splatting
-	    $ContextSplatting = @{ }
-	    if ($PSBoundParameters['Credential']){
-	        Write-Verbose "[$FunctionName] Found Credential Parameter"
-	        $ContextSplatting.Credential = $Credential
-	    }
-	    if ($PSBoundParameters['DomainName']){
-	        Write-Verbose "[$FunctionName] Found DomainName Parameter"
-	        $ContextSplatting.DomainName = $DomainName
-		}
-		
-		#Get Identity Type
-		$Object = Get-ADSIObject -Identity $Identity @ContextSplatting
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param(
+        [Parameter(Mandatory = $true,
+            Position = 0,
+            ParameterSetName = "Identity")]
+        [System.string]$Identity,
 
-		switch -Wildcard ($Object.objectclass){
-			"*group" {$IdentityType = "Group"}
-			"*computer" {$IdentityType = "Computer"}
-			"*user" {$IdentityType = "User"}
-		}
-	}
-	
-	process{
+        [Alias("RunAs")]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
 
-		function Get-RecursiveGroups([System.DirectoryServices.AccountManagement.Principal]$Group){
-			Write-Output "$("  "*$Spacecount)-$($Group.Name)"
-			$MemberOf = $Group.GetGroups()
-			If($null -ne $MemberOf){
-				foreach($MemberOfGroup in $MemberOf){
-					$Spacecount ++
-					Get-RecursiveGroups -Group $MemberOfGroup
-				}
-			}
-		}
+        [System.String]$DomainName
+    )
 
-	    #GetGroups
-	    If($IdentityType -eq "User"){
-	        $Groups = (Get-ADSIUser -Identity $Identity @ContextSplatting).GetGroups()
-	        Write-Verbose "[$FunctionName] Type: User"
-	    } elseif ($IdentityType -eq "Computer") {
-	        $Groups = (Get-ADSIComputer -Identity $Identity @ContextSplatting).GetGroups()
-	        Write-Verbose "[$FunctionName] Type: Computer"
-	    } elseif ($IdentityType -eq "Group") {
-	        $Groups = (Get-ADSIGroup -Identity $Identity @ContextSplatting).GetGroups()
-	        Write-Verbose "[$FunctionName] Type: Group"
-	    }
-        
+    begin{
+        $FunctionName = (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly).Mycommand
+
+        # Create Context splatting
+        $ContextSplatting = @{ }
+        if ($PSBoundParameters['Credential']){
+            Write-Verbose "[$FunctionName] Found Credential Parameter"
+            $ContextSplatting.Credential = $Credential
+        }
+        if ($PSBoundParameters['DomainName']){
+            Write-Verbose "[$FunctionName] Found DomainName Parameter"
+            $ContextSplatting.DomainName = $DomainName
+        }
+
+        #Get Identity Type
+        $Object = Get-ADSIObject -Identity $Identity @ContextSplatting
+
+        switch -Wildcard ($Object.objectclass){
+            "*group" {$IdentityType = "Group"}
+            "*computer" {$IdentityType = "Computer"}
+            "*user" {$IdentityType = "User"}
+        }
+    }
+
+    process{
+
+        function Get-RecursiveGroups([System.DirectoryServices.AccountManagement.Principal]$Group){
+            Write-Output "$("  "*$Spacecount)-$($Group.Name)"
+            $MemberOf = $Group.GetGroups()
+            If($null -ne $MemberOf){
+                foreach($MemberOfGroup in $MemberOf){
+                    $Spacecount ++
+                    Get-RecursiveGroups -Group $MemberOfGroup
+                }
+            }
+        }
+
+        #GetGroups
+        If($IdentityType -eq "User"){
+            $Groups = (Get-ADSIUser -Identity $Identity @ContextSplatting).GetGroups()
+            Write-Verbose "[$FunctionName] Type: User"
+        } elseif ($IdentityType -eq "Computer") {
+            $Groups = (Get-ADSIComputer -Identity $Identity @ContextSplatting).GetGroups()
+            Write-Verbose "[$FunctionName] Type: Computer"
+        } elseif ($IdentityType -eq "Group") {
+            $Groups = (Get-ADSIGroup -Identity $Identity @ContextSplatting).GetGroups()
+            Write-Verbose "[$FunctionName] Type: Group"
+        }
+
         if($Groups){
-			Write-Verbose "Groups found: $($Groups.Name)"
-			Write-Output $(Get-ADSIUser -Identity $Identity).Name
+            Write-Verbose "Groups found: $($Groups.Name)"
+            Write-Output $(Get-ADSIUser -Identity $Identity).Name
             foreach($Group in $Groups){
-				$Spacecount = 1
-				Get-RecursiveGroups -Group $Group
+                $Spacecount = 1
+                Get-RecursiveGroups -Group $Group
             }
         } else {
             Write-Verbose "No Groups found"
         }
-	}
+    }
 }
