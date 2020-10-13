5303406e6f6dfb46641b52d428f8b25035923814
Skoetting <github@skoetting.net>
2020-10-13 13:01:54 -0700

    Rework Reset-ADSIUserPasswordage

--- src/public/Reset-ADSIUserPasswordAge.ps1
+++ src/public/Reset-ADSIUserPasswordAge.ps1
@@ -10,6 +10,9 @@ function Reset-ADSIUserPasswordAge
 .PARAMETER Identity
     Specifies the Identity
 
+.PARAMETER DomainName
+    Specify the Domain Distinguished name
+
 .PARAMETER Credential
     Specifies alternative credential
 
@@ -26,19 +29,48 @@ function Reset-ADSIUserPasswordAge
     [CmdletBinding(SupportsShouldProcess = $true)]
     param
     (
-        [Parameter(Mandatory = $true)]
-        [string]$Identity,
+        [Parameter(Mandatory = $true, ValueFromPipeline)]
+        $Identity,
+
+        [Alias("Domain", "DomainDN")]
+        [String]$DomainName = $(([adsisearcher]"").Searchroot.path),
 
         [Alias("RunAs")]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
+
+    begin {
+        $FunctionName = (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly).Mycommand
+
+        # Create Context splatting
+        $ContextSplatting = @{}
+
+        if ($PSBoundParameters['Credential'])
+        {
+            Write-Verbose "[$FunctionName] Found Credential Parameter"
+            $ContextSplatting.Credential = $Credential
+        }
+        if ($PSBoundParameters['DomainName'])
+        {
+            Write-Verbose "[$FunctionName] Found DomainName Parameter"
+            $ContextSplatting.DomainName = $DomainName
+        }
+    }
+
     process
     {
-        if ($pscmdlet.ShouldProcess("$Identity", "Change Account Password"))
-        {
-            (Get-ADSIUser @PSBoundParameters).RefreshExpiredPassword()
+        if($Identity.GetType().FullName -eq 'System.String') {
+            if ($pscmdlet.ShouldProcess("$Identity", "Change Account Password"))
+            {
+                (Get-ADSIUser -Identity $Identity @ContextSplatting).RefreshExpiredPassword()
+            }
+        } else {
+            if ($pscmdlet.ShouldProcess("$($Identity.SamAccountName)", "Change Account Password"))
+            {
+                $Identity.RefreshExpiredPassword()
+            }
         }
     }
 }
