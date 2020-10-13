9f76a14e7fb5147b4ca0951d16e80cd5393cebef
Skoetting <github@skoetting.net>
2020-10-13 12:44:56 -0700

    Rework Test-ADSIUserIsLockedOut

--- src/public/Test-ADSIUserIsLockedOut.ps1
+++ src/public/Test-ADSIUserIsLockedOut.ps1
@@ -10,8 +10,11 @@ function Test-ADSIUserIsLockedOut
 .PARAMETER Identity
     Specifies the Identity
 
+.PARAMETER DomainName
+    Specify the Domain Distinguished name
+
 .PARAMETER Credential
-    Specifies alternative credential
+    Specify alternative Credential
 
 .EXAMPLE
     Test-ADSIUserIsLockedOut -Identity 'testaccount'
@@ -27,16 +30,50 @@ function Test-ADSIUserIsLockedOut
     [OutputType('System.Boolean')]
     param
     (
-        [Parameter(Mandatory = $true)]
-        [string]$Identity,
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        $Identity,
+
+        [Alias("Domain", "DomainDN")]
+        [String]$DomainName = $(([adsisearcher]"").Searchroot.path),
 
         [Alias("RunAs")]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
         $Credential = [System.Management.Automation.PSCredential]::Empty
     )
-    process
-    {
-        (Get-ADSIUser @PSBoundParameters).IsAccountLockedOut()
+
+    begin {
+        $FunctionName = (Get-Variable -Name MyInvocation -Scope 0 -ValueOnly).Mycommand
+
+        # Create Context splatting
+        $ContextSplatting = @{}
+
+        if ($PSBoundParameters['Credential'])
+        {
+            Write-Verbose "[$FunctionName] Found Credential Parameter"
+            $ContextSplatting.Credential = $Credential
+        }
+        if ($PSBoundParameters['DomainName'])
+        {
+            Write-Verbose "[$FunctionName] Found DomainName Parameter"
+            $ContextSplatting.DomainName = $DomainName
+        }
+    }
+
+    process {
+
+        if($Identity.GetType().FullName -eq 'System.String') {
+            $User = Get-ADSIUser -Identity $Identity @ContextSplatting
+            if($User -eq $null){
+                Write-Error "[$FunctionName] Could not find User"
+            } else {
+                Write-Verbose "[$FunctionName] Found User"
+            }
+        } else {
+            Write-Verbose "[$FunctionName] Found User"
+            $User = $Identity
+        }
+
+        $User.IsAccountLockedOut()
     }
 }
